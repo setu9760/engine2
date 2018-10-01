88405f3ee8969dfd39fd6d717e7af71dca550e5a
Setu Desai <setu.desai@smartstream-stp.com>
2018-10-01 16:30:16 +0100

    remove spring packaging and fallback to traditional war package.

--- src/main/java/com/mycompany/engine2/Engine2Controller.java
+++ src/main/java/com/mycompany/engine2/Engine2Controller.java
@@ -1,5 +1,8 @@
 package com.mycompany.engine2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
@@ -16,10 +19,19 @@ import com.mycompany.app.common.model.ModelB;
 
 @RestController
 @RequestMapping(path = "/api", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
-public class Engine2Controller {
+public class Engine2Controller implements InitializingBean {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(Engine2Controller.class);
+	
 	private RestTemplate restTemplate;
-	@Value("${engine1.url:http://localhost:8081/}")
+	
+	@Value("${engine1.protocol:http}")
+	private String engine1Protocol;
+	@Value("${engine1.host:localhost}")
+	private String engine1Host;
+	@Value("${engine1.port:8081}")
+	private int engine1Port;
+	
 	private String engine1Url;
 	
 	@Autowired
@@ -27,21 +39,27 @@ public class Engine2Controller {
 		this.restTemplate = restTemplate;
 	}
 	
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		engine1Url = String.format("%s://%s:%s/engine1/api/", engine1Protocol, engine1Host, engine1Port);
+		LOGGER.info("Using '{}' for making rest requests to engine1", engine1Url);
+	}
+	
 	@GetMapping("/hello/{name}")
 	public ResponseEntity<String> hello(@PathVariable String name) {
-		String s = restTemplate.getForObject(engine1Url + "engine1/api/hello/" + name, String.class);
+		String s = restTemplate.getForObject(engine1Url + "hello/" + name, String.class);
 		return new ResponseEntity<>(s, HttpStatus.OK) ;
 	}
 
 	@GetMapping("/modela/{name}")
 	public ResponseEntity<ModelA> getModelA(@PathVariable String name) throws Exception {
-		ResponseEntity<ModelA> entity = restTemplate.getForEntity(engine1Url + "engine1/api/modela/" + name, ModelA.class);
+		ResponseEntity<ModelA> entity = restTemplate.getForEntity(engine1Url + "modela/" + name, ModelA.class);
 		return entity;
 	}
 
 	@GetMapping("/modelb/{name}")
 	public ResponseEntity<ModelB> getModelB(@PathVariable String name) throws Exception {
-		ResponseEntity<ModelB> entity = restTemplate.getForEntity(engine1Url + "engine1/api/modelb/" + name, ModelB.class);
+		ResponseEntity<ModelB> entity = restTemplate.getForEntity(engine1Url + "modelb/" + name, ModelB.class);
 		return entity;
 	}
 }
